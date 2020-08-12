0682d9a722c2885aff2d49deebcfd78ed83fcb21
J Mulder <jlmulder2014@gmail.com>
2020-08-12 00:07:09 -0700

    updates

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 //collecting from the user model and exporting user data
 const User = require('./User');
+const Post = require("./Post");
 
 //exporting object with user model as a property
-module.exports = { User };
+module.exports = { User, Post };
