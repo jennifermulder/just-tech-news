89ee65fe9e6ecb795188b98b7e943160bb40ddbf
J Mulder <jlmulder2014@gmail.com>
2020-08-12 10:10:23 -0700

    update .env/ connection

--- models/index.js
+++ models/index.js
@@ -2,5 +2,14 @@
 const User = require('./User');
 const Post = require("./Post");
 
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
 //exporting object with user model as a property
 module.exports = { User, Post };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./routes/index.js');
 //importing sequelize connection that is in connection.js
 const sequelize = require('./config/connection');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,6 +16,7 @@ app.use(routes);
 // turn on connection to db and server
 //.sync mthod is sequalize taking the models and connecting them to associated database tables. IF it doesn't find one it will create one
 //"force: true" will drop and re-create all database tables on startup
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'))
+    ;
+}).catch(err => console.log(err))
