1158471ef91d5b16f720e3850a0774063464d354
J Mulder <jlmulder2014@gmail.com>
2020-08-13 17:00:52 -0700

    connect to jaws DB

--- config/connection.js
+++ config/connection.js
@@ -8,11 +8,16 @@ require('dotenv').config();
 // "new sequalize" = new connection
 // console.log(process.env.DB_NAME, "data base name here", process.env.DB_PW, process.env.DB_USER);
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  //configuration settings
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+let sequelize;
+//use DB first, if not, use local host
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
